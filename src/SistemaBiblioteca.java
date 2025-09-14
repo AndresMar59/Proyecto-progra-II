737527c33b69def42648e703b83838a924046789
LiHatake <lit03@outlook.es>
2025-09-14 08:29:25 -0600

    Modificacion de Menu y agregue registrio de usuarios a biblioteca

--- src/SistemaBiblioteca.java
+++ src/SistemaBiblioteca.java
@@ -2,28 +2,37 @@ import java.util.Scanner;
 
 public class SistemaBiblioteca {
     public static void main(String[] args) {
+        biblioteca biblio = new biblioteca();
+
+        //Cargar datos al iniciar - Opcion 5
+        biblio.cargarUsuario("usuarios.txt");
+
+        Scanner getinfo = new Scanner(System.in);
         int opcion = -1;
+
+
 
         while (true) {
             /**Menu*/
             System.out.println("Bienvenidos al sistema de biblioteca");
             System.out.println("--Seleccione una opcion del menu:--");
-            System.out.println("1.Registrar Libro");
-            System.out.println("2.Registrar Usuario");
-            System.out.println("3.Realizar prestamo");
-            System.out.println("4. Ver lista de libros");
-            System.out.println("5. Salir");
+            System.out.println("1. Registrar Libro");
+            System.out.println("2. Registrar Usuario");
+            System.out.println("3. Realizar prestamo");
+            System.out.println("4. Devolver Libro");
+            System.out.println("5. Listado de Libros");
+            System.out.println("6. Salir");
             System.out.println("-------------------------------------------------------");
-            Scanner eleccion = new Scanner(System.in);
 
             //System.out.println(opcion); //Verificar que funciona
 
             try {
-                System.out.print("Opcion: "); opcion = eleccion.nextInt();
+                System.out.print("Opcion: "); opcion = getinfo.nextInt();
+                getinfo.nextLine();
             } catch (java.util.InputMismatchException e) {
                 System.out.println("**Opcion invalida** Seleccione de nuevo!");
                 System.out.println("-------------------------------------------------------");
-                eleccion.next();
+                getinfo.next();
                 opcion = -1;
                 continue;
             }
@@ -33,8 +42,20 @@ public class SistemaBiblioteca {
                     System.out.println("Ha seleccionado 1");
                     return;
                 case 2:
-                    System.out.println("Ha seleccionado 2");
-                    return;
+                    System.out.println("Registro de Usuario");
+                    System.out.print("Ingrese el nombre del usuario:");
+                    String nombre = getinfo.nextLine();
+                    System.out.print("Ingrese ID:");
+                    String Id = getinfo.nextLine();
+                    //Prueba
+                    //System.out.println("Nombre del usuario: " + nombre + "ID: " + Id);
+
+                    //Registro en un archivo txt
+                    biblio.registrarUsuario(nombre, Id);
+                    biblio.guardarUsuario("usuarios.txt");
+
+
+                    break;
                 case 3:
                     System.out.println("Ha seleccionado 3");
                     return;
@@ -42,7 +63,10 @@ public class SistemaBiblioteca {
                     System.out.println("Ha seleccionado 4");
                     return;
                 case 5:
+                    System.out.println("Ha seleccionado 5");
                     return;
+                case 6:
+                        return;
                 default:
                     System.out.println("**Opcion invalida** Seleccione de nuevo!");
                     System.out.println("-------------------------------------------------------");
--- src/biblioteca.java
+++ src/biblioteca.java
@@ -1,3 +1,49 @@
+import java.util.ArrayList;
+import java.io.*; //Para manero de ingreso y salida de datos para el literal 5
+
 public class biblioteca {
+
+    private ArrayList<usuario> usuarios;
+
+    public biblioteca() {
+
+        usuarios = new ArrayList<>();
+    }
+
+    public void registrarUsuario(String nombre, String Id) {
+        usuarios.add(new usuario(nombre,Id));
+    }
+
+    public void guardarUsuario(String archivo) {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(archivo))){
+            for (usuario u : usuarios) {
+                bw.write(u.getId() + ";" + u.getNombre());
+                bw.newLine();
+            }
+
+            System.out.println("Usuario guardado. en documento " + archivo);
+        }catch(IOException e){
+            System.out.println("Error" + e.getMessage());
+        }
+    }
+
+    //Cargar Usuario
+    public void cargarUsuario(String archivo) {
+        try(BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String line;
+            while ((line = br.readLine()) != null){
+            String[] data = line.split(";");
+            if(data.length == 2){
+                usuarios.add(new usuario(data[1],data[0]));
+            }
+        }
+            System.out.println("Usuario cargado en documento " + archivo);
+    }catch (IOException e){
+            System.out.println("Error" + e.getMessage());
+        }
+}
+
+
+
 
 }
